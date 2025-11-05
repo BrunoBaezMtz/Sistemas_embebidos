4c93d8d35ef2f51451470084bdd0da5b22e2130e
Bruno Baez <brunomtz231@gmail.com>
2025-11-05 07:57:20 -0600

    05/11

---
+++ ADC.X/adc.c
@@ -0,0 +1,31 @@
+
+//file: adc
+// autor: BRuno
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000 //20MHz
+
+
+void adc_init(void){
+    TRISA = 0b00000001; //pin AN0
+    
+    ADCON1 = 0b11001110;
+    
+    /*
+     bit 0 ADCON = 1 - enciende el ADC
+     bit 2 godone = 0
+     bit 3-5  canal 0
+     bit 6-7 110
+     */
+    ADCON0 = 0b10000001;
+    
+    __delay_us(20);
+}
+
+unsigned int adc_read_an0(void){
+    __delay_us(20);
+    GO_nDONE = 1;
+    while(GO_nDONE);
+    return ((unsigned int)ADRESH<<8|ADRESL);
+    
+}
