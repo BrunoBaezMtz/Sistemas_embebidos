8dcfe72fdee28e3d9ac4a68396e12260cdc9a715
Bruno Baez <brunomtz231@gmail.com>
2025-11-06 00:56:05 -0600

    commit 06/11/25 12:55am

--- display_7segmentos.X/counter_1.c
+++ display_7segmentos.X/counter_1.c
@@ -2,54 +2,52 @@
 #define _XTAL_FREQ 20000000  // Frecuencia del cristal (ajústala a tu caso real)
 #include <xc.h>
 
-unsigned char counter = 0;
+//Array con los codigos de 7 segmentos para los numeros 0 a 9
 unsigned char NUMBERS[10] = {
-    0x3F,
-    0x06,
-    0x5B,
-    0x4F,
-    0x66,
-    0x6D,
-    0x7D,
-    0x07,
-    0x7F,
-    0x6F,
-    0x00,
+    0x3F, //0
+    0x06, //1
+    0x5B, //2
+    0x4F, //3
+    0x66, //4
+    0x6D, //5
+    0x7D, //6
+    0x07, //7
+    0x7F, //8
+    0x6F  //9
     
 }
 
 void main(void) {
     TRISA = 0x01;   // RA0 como entrada
     TRISB = 0x00;   // Puerto B como salida
-    PORTB = 0x3F;   // Mostrar "0" inicialmente
+    PORTB = NUMBERS[0];   // Mostrar "0" inicialmente usando array
+    
+    unsigned char counter = 0; // Se inicializa aqui
     
     while(1) {
-        unsigned char button = PORTAbits.RA0;
-        
-        if(button == 1) {
-            __delay_ms(200);   // Antirrebote simple
-            counter++;
+        //1. deteccion de pulsacion
+        if(PORTAbits.RA0 == 1){
+            /*2. antirrebote inicial
+             * retardo de 50ms
+            */
+            __delay_ms(50);
             
-            if(counter == 10) {
-                counter = 0;
+            //3.verificar que el boton sigue presionado despues del antirebote
+            if(PORTAbits.RA0 == 1){
+                //logica de conteo:
+                counter++;
+                if(counter >= 10){
+                    counter = 0;
+                }
+                
+                //4. actualizar el display
+                //el valor de 'counter' se usa como indice para el array NUMBERS
+                PORTB = NUMBERS[counter];
+                
+                //5. esperar a que se suelte el boton
+                //aqui se asegura que solo s cuente una vez
+                while(PORTAbits.RA0 == 1);
             }
-            
-            switch(counter) {
-                case 0: PORTB = 0x3F; break; // 0
-                case 1: PORTB = 0x06; break; // 1
-                case 2: PORTB = 0x5B; break; // 2
-                case 3: PORTB = 0x4F; break; // 3
-                case 4: PORTB = 0x66; break; // 4
-                case 5: PORTB = 0x6D; break; // 5
-                case 6: PORTB = 0x7D; break; // 6
-                case 7: PORTB = 0x07; break; // 7
-                case 8: PORTB = 0x7F; break; // 8
-                case 9: PORTB = 0x6F; break; // 9
-                default: PORTB = 0x00; break;
-            }
-            
-            // Esperar a que se suelte el botón
-            while(PORTAbits.RA0 == 1);
         }
     }
 }
